8dceb152b107c8fb9be8813ec4f436638bae28be
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2022-03-01 01:21:06 -0500

    Removed xml file due to it is no longer needed; besides, remove the SetContentView in the because the Android Manifest is applying the new theme.

--- app/src/main/java/com/test/splashscreenwiththeme/MainActivity.kt
+++ app/src/main/java/com/test/splashscreenwiththeme/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.test.splashscreenwiththeme
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 }
